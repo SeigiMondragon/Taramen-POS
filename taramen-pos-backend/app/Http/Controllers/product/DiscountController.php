bdc77819b68d52c26b15295064a0b175b358748e
SeigiMondragon <sonic92yeylol@gmail.com>
2026-01-17 18:36:00 +0800

    Complete Discount Controller

--- taramen-pos-backend/app/Http/Controllers/product/DiscountController.php
+++ taramen-pos-backend/app/Http/Controllers/product/DiscountController.php
@@ -2,38 +2,67 @@
 
 namespace App\Http\Product\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\DiscountRequest;
 use App\Http\Controllers\Controller;
 use App\Models\Discount;
+
 
 class DiscountController extends Controller
 {
 
     public function index()
     {
-    
+        $discounts = Discount::all();
+        return response()->json([
+            'data' => $discounts
+        ],200);
     }
 
 
-    public function store(Request $request)
+    public function store(DiscountRequest $request)
     {
-        
+        $validated_data = $request->validate();
+        $created_discount = Discount::create($validated_data);
+        return response()->json([
+            'message' => "Discount has been created successfully",
+            'discount' => $created_discount
+        ],201);
+
     }
 
 
-    public function show(string $id)
+    public function show( $id)
     {
-        //
+        $discount = Discount::findOrFail($id);
+        return response()->json([
+            'message' => 'discount found',
+            'discount' => $discount
+        ],200);
     }
 
 
-    public function update(Request $request, string $id)
+    public function update(DiscountRequest $request,  $id)
     {
-        //
+        $discount = Discount::findOrFail($id);
+        $validated_data = $request->validate();
+        $discount->update($validated_data);
+
+        return response()->json([
+            'message' => 'discount has been updated successfully',
+            'updated_discount' => $validated_data
+        ],201);
+
     }
 
-    public function destroy(string $id)
+    public function destroy( $id)
     {
-        //
+        $discount = Discount::findOrFail($id);
+        $discount->delete();
+
+        return response()->json([
+            'message' => 'Discount deleted successfully',
+            'category' => $discount
+        ], 200);
+
     }
 }
